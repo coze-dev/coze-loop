f92a32dab342add961871d3a45d97ae3ba0c9b8f
yanghoule <yanghoule@bytedance.com>
2025-09-03 15:48:54 +0800

    dev

--- backend/modules/llm/domain/entity/common.go
+++ backend/modules/llm/domain/entity/common.go
@@ -6,10 +6,11 @@ package entity
 type Scenario string
 
 const (
-	ScenarioDefault     Scenario = "default"
-	ScenarioPromptDebug Scenario = "prompt_debug"
-	ScenarioEvalTarget  Scenario = "eval_target"
-	ScenarioEvaluator   Scenario = "evaluator"
+	ScenarioDefault          Scenario = "default"
+	ScenarioPromptDebug      Scenario = "prompt_debug"
+	ScenarioEvalTarget       Scenario = "eval_target"
+	ScenarioEvaluator        Scenario = "evaluator"
+	ScenarioPromptAsAService Scenario = "prompt_as_a_service"
 )
 
 func ScenarioValue(scenario *Scenario) Scenario {
