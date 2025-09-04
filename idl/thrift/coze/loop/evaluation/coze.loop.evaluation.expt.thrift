4dfb921cfb6cd6671b17a5dcf39d99b4d1c5dedb
Han Xu <hanxu.ee@bytedance.com>
2025-09-04 19:08:30 +0800

    update

--- idl/thrift/coze/loop/evaluation/coze.loop.evaluation.expt.thrift
+++ idl/thrift/coze/loop/evaluation/coze.loop.evaluation.expt.thrift
@@ -515,7 +515,7 @@ struct DeleteExptInsightAnalysisRecordResponse {
 struct FeedbackExptInsightAnalysisReportRequest {
     1: required i64 workspace_id (api.body = 'workspace_id', api.js_conv = 'true', go.tag = 'json:"workspace_id"')
     2: required i64 expt_id (api.path = 'expt_id' , api.js_conv = 'true', go.tag = 'json:"expt_id"')
-    3: required i64 insight_analysis_record_id (api.path = 'export_id', api.js_conv = 'true', go.tag = 'json:"export_id"')
+    3: required i64 insight_analysis_record_id (api.path = 'insight_analysis_record_id', api.js_conv = 'true', go.tag = 'json:"insight_analysis_record_id"')
     4: required expt.FeedbackActionType FeedbackActionType
     5: optional string comment
 
