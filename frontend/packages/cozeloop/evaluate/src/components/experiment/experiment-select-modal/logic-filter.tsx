46071d4b35a0cac3007ac4b6bbaf6266a0b0f276
yangfeng.alanyf <yangfeng.alanyf@bytedance.com>
2025-09-05 14:40:15 +0800

    feat(evaluate): update evaluate i18n text

--- frontend/packages/cozeloop/evaluate/src/components/experiment/experiment-select-modal/logic-filter.tsx
+++ frontend/packages/cozeloop/evaluate/src/components/experiment/experiment-select-modal/logic-filter.tsx
@@ -1,14 +1,4 @@
-import { I18n } from '@cozeloop/i18n-adapter';
-import {
-  LogicEditor,
-  type LogicField,
-  type LogicFilter,
-} from '@cozeloop/evaluate-components';
-import {
-  type ExptStatus,
-  FieldType,
-  type Evaluator,
-} from '@cozeloop/api-schema/evaluation';
+import { type ExptStatus, FieldType } from '@cozeloop/api-schema/evaluation';
 
 export interface Filter {
   name?: string;
@@ -26,71 +16,3 @@ export const filterFields: { key: keyof Filter; type: FieldType }[] = [
     type: FieldType.EvalSetID,
   },
 ];
-
-export default function ExperimentLogicFilter({
-  logicFilter,
-  evaluators = [],
-  onChange,
-  onClose,
-}: {
-  logicFilter: LogicFilter | undefined;
-  evaluators: Evaluator[] | undefined;
-  onChange: (newData?: LogicFilter) => void;
-  onClose?: () => void;
-}) {
-  const logicFields: LogicField[] = [
-    {
-      title: I18n.t('creator'),
-      name: 'created_by',
-      type: 'options',
-      setterProps: {
-        optionList: [
-          { label: I18n.t('user_zhangsan'), value: 1 },
-          { label: I18n.t('user_lisi'), value: 2 },
-          { label: I18n.t('user_wangwu'), value: 3 },
-        ],
-      },
-    },
-    {
-      title: I18n.t('evaluation_object_type'),
-      name: 'eval_target_type',
-      type: 'options',
-      setterProps: {
-        optionList: [
-          { label: 'Prompt', value: 1 },
-          { label: I18n.t('coze_agent'), value: 2 },
-        ],
-      },
-    },
-    {
-      title: I18n.t('evaluation_object'),
-      name: 'eval_target',
-      type: 'options',
-      setterProps: {
-        optionList: [
-          { label: I18n.t('encyclopedia_expert'), value: 1 },
-          { label: I18n.t('joke_king'), value: 2 },
-        ],
-      },
-    },
-    ...evaluators.map(evaluator => {
-      const field: LogicField = {
-        title: evaluator.name ?? '',
-        name: `${evaluator.evaluator_id ?? ''}`,
-        type: 'number' as const,
-        setterProps: {
-          step: 0.1,
-        },
-      };
-      return field;
-    }),
-  ];
-  return (
-    <LogicEditor
-      fields={logicFields}
-      value={logicFilter}
-      onChange={onChange}
-      onClose={onClose}
-    />
-  );
-}
