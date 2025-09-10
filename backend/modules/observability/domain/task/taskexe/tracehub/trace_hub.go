61ed11f3a6d9863de6c8364739511df826ccd874
zhaoxun <zhaoxun.3233@bytedance.com>
2025-09-10 17:05:58 +0800

    fix

--- backend/modules/observability/domain/task/taskexe/tracehub/trace_hub.go
+++ backend/modules/observability/domain/task/taskexe/tracehub/trace_hub.go
@@ -36,7 +36,7 @@ func NewTraceHubImpl(
 	evaluationService rpc.IEvaluationRPCAdapter,
 ) (ITraceHubService, error) {
 	processor.InitProcessor(datasetServiceProvider, evalService, evaluationService, tRepo)
-	ticker := time.NewTicker(5 * time.Minute) // 每x分钟执行一次定时任务
+	ticker := time.NewTicker(1 * time.Minute) // 每x分钟执行一次定时任务
 	impl := &TraceHubServiceImpl{
 		taskRepo: tRepo,
 		ticker:   ticker,
