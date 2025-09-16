717c9821cc8e4b38435b2bfbff4716bd4ca3c1f6
zhaoxun <zhaoxun.3233@bytedance.com>
2025-09-16 14:43:57 +0800

    feat:fix

--- backend/modules/observability/domain/task/service/task_service.go
+++ backend/modules/observability/domain/task/service/task_service.go
@@ -125,15 +125,18 @@ func (t *TaskServiceImpl) CreateTask(ctx context.Context, req *CreateTaskReq) (r
 	taskConfig := tconv.TaskPO2DTO(ctx, taskPO, nil)
 	// 数据回流任务——创建/更新输出数据集
 	// 自动评测历史回溯——创建空壳子
+	taskOp := taskexe.TaskOpUndefined
 	if req.Task.GetTaskType() == task.TaskTypeAutoDataReflow {
-		taskOp := taskexe.TaskOpNewData
-		if t.shouldTriggerBackfill(req.Task) {
-			taskOp = taskexe.TaskOpCreateBackfill
-			if err = proc.OnChangeProcessor(ctx, taskConfig, taskOp); err != nil {
-				logs.CtxError(ctx, "create initial task run failed, task_id=%d, err=%v", id, err)
-				//任务改为禁用？
-				return nil, err
-			}
+		taskOp = taskexe.TaskOpNewData
+	}
+	if t.shouldTriggerBackfill(req.Task) {
+		taskOp = taskexe.TaskOpCreateBackfill
+	}
+	if taskOp != taskexe.TaskOpUndefined {
+		if err = proc.OnChangeProcessor(ctx, taskConfig, taskOp); err != nil {
+			logs.CtxError(ctx, "create initial task run failed, task_id=%d, err=%v", id, err)
+			//任务改为禁用？
+			return nil, err
 		}
 	}
 	// TODO: 历史回溯数据发MQ
