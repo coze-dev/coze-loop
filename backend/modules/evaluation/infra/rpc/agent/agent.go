27c16ec13bd2050aae44d184cac6801305394019
Han Xu <hanxu.ee@bytedance.com>
2025-09-18 20:36:28 +0800

    update

--- backend/modules/evaluation/infra/rpc/agent/agent.go
+++ backend/modules/evaluation/infra/rpc/agent/agent.go
@@ -5,10 +5,11 @@ package agent
 
 import (
 	"context"
-	"errors"
 
 	"github.com/coze-dev/coze-loop/backend/modules/evaluation/domain/component/rpc"
 	"github.com/coze-dev/coze-loop/backend/modules/evaluation/domain/entity"
+	"github.com/coze-dev/coze-loop/backend/modules/evaluation/pkg/errno"
+	"github.com/coze-dev/coze-loop/backend/pkg/errorx"
 )
 
 type AgentAdapter struct{}
@@ -18,9 +19,9 @@ func NewAgentAdapter() rpc.IAgentAdapter {
 }
 
 func (a AgentAdapter) CallTraceAgent(ctx context.Context, spaceID int64, url string) (int64, error) {
-	return 0, errors.New("CallTraceAgent not implement")
+	return 0, errorx.NewByCode(errno.CommonInternalErrorCode, errorx.WithExtraMsg("CallTraceAgent not implement"))
 }
 
 func (a AgentAdapter) GetReport(ctx context.Context, spaceID, reportID int64) (report string, status entity.ReportStatus, err error) {
-	return "", 0, errors.New("CallTraceAgent not implement")
+	return "", 0, errorx.NewByCode(errno.CommonInternalErrorCode, errorx.WithExtraMsg("GetReport not implement"))
 }
