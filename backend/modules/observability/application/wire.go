0401556c5dbb5f6e121ee1c7b935be1362515ff0
zhaoxun <zhaoxun.3233@bytedance.com>
2025-09-24 15:44:51 +0800

    fix

--- backend/modules/observability/application/wire.go
+++ backend/modules/observability/application/wire.go
@@ -266,7 +266,8 @@ func InitTaskApplication(
 	exptService experimentservice.Client,
 	datasetService datasetservice.Client,
 	benefit benefit.IBenefitService,
-	fileClient fileservice.Client) (ITaskApplication, error) {
+	fileClient fileservice.Client,
+	taskProcessor task_processor.TaskProcessor) (ITaskApplication, error) {
 	wire.Build(taskSet)
 	return nil, nil
 }
