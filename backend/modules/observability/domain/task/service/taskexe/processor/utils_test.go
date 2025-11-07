53cdb67a3f7725c087d9a8787b862784f435c0f8
zhaoxun.3233 <zhaoxun.3233@bytedance.com>
2025-11-07 17:21:08 +0800

    fix ut

--- backend/modules/observability/domain/task/service/taskexe/processor/utils_test.go
+++ backend/modules/observability/domain/task/service/taskexe/processor/utils_test.go
@@ -279,11 +279,11 @@ func TestBuildItems(t *testing.T) {
 	goodSpan := &loop_span.Span{TraceID: "1234567890abcdef1234567890abcdef", SpanID: "deadc0debeefcafe", Input: "hello"}
 	badSpan := &loop_span.Span{TraceID: goodSpan.TraceID, SpanID: "badbadbadbadbad", Input: "invalid"}
 	mapping.FieldSchema.ContentType = gptr.Of(common.ContentTypeMultiPart)
-	multipartSchema := []*eval_set.FieldSchema{
+	multipartSchema := []*entity.FieldSchema{
 		{
 			Key:         gptr.Of("field_1"),
-			Name:        gptr.Of("field_1"),
-			ContentType: gptr.Of(common.ContentTypeMultiPart),
+			Name:        "field_1",
+			ContentType: common.ContentTypeMultiPart,
 		},
 	}
 	multipartBytes, err := json.Marshal(multipartSchema)
