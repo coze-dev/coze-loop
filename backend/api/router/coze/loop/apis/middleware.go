3174aff0b61e3bf5757a8e91f888d073f8791808
caijialin.626 <caijialin.626@bytedance.com>
2025-11-11 19:42:39 +0800

    [feat][prompt] fix conflict

--- backend/api/router/coze/loop/apis/middleware.go
+++ backend/api/router/coze/loop/apis/middleware.go
@@ -1645,3 +1645,8 @@ func _updateevaluationsetoapiMw(handler *apis.APIHandler) []app.HandlerFunc {
 	// your code...
 	return nil
 }
+
+func _listpromptbasicMw(handler *apis.APIHandler) []app.HandlerFunc {
+	// your code...
+	return nil
+}
