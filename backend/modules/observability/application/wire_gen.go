18da4795e32fffad92732219fed70fc14a798911
zhaoxun.3233 <zhaoxun.3233@bytedance.com>
2025-11-19 18:08:39 +0800

    add wire

--- backend/modules/observability/application/wire_gen.go
+++ backend/modules/observability/application/wire_gen.go
@@ -304,7 +304,7 @@ func InitTaskApplication(db2 db.Provider, idgen2 idgen.IIDGenerator, configFacto
 		return nil, err
 	}
 	iTaskCallbackService := service3.NewTaskCallbackServiceImpl(iTaskRepo, iTraceRepo, taskProcessor, iTenantProvider, iTraceConfig, benefit2)
-	v := NewScheduledTask(iLocker, iTraceConfig, iTraceHubService, iTaskService, taskProcessor, iTaskRepo, aid)
+	v := NewScheduledTask(iLocker, iTraceConfig, iTraceHubService, iTaskService, taskProcessor, iTaskRepo)
 	iTaskApplication, err := NewTaskApplication(iTaskService, iAuthProvider, iEvaluatorRPCAdapter, iEvaluationRPCAdapter, iUserProvider, iTraceHubService, taskProcessor, iTaskCallbackService, v, iTraceRepo)
 	if err != nil {
 		return nil, err
@@ -402,7 +402,6 @@ func NewScheduledTask(
 	taskService service3.ITaskService,
 	taskProcessor processor.TaskProcessor,
 	taskRepo repo3.ITaskRepo,
-	aid int32,
 ) []scheduledtask.ScheduledTask {
-	return []scheduledtask.ScheduledTask{scheduledtask2.NewStatusCheckTask(locker, config3, traceHubService, taskService, taskProcessor, taskRepo, aid), scheduledtask2.NewLocalCacheRefreshTask(traceHubService, taskRepo)}
+	return []scheduledtask.ScheduledTask{scheduledtask2.NewStatusCheckTask(locker, config3, traceHubService, taskService, taskProcessor, taskRepo), scheduledtask2.NewLocalCacheRefreshTask(traceHubService, taskRepo)}
 }
