c413467ae2aed136f34a398b76eae5993206afb1
zhaoxun.3233 <zhaoxun.3233@bytedance.com>
2025-11-19 22:00:04 +0800

    fix ut

--- backend/modules/observability/domain/component/scheduledtask/scheduledtask.go
+++ backend/modules/observability/domain/component/scheduledtask/scheduledtask.go
@@ -40,9 +40,11 @@ func (b *BaseScheduledTask) Run() error {
 				ctx := context.Background()
 				startTime := time.Now()
 				if err := b.RunOnce(ctx); err != nil {
-					logs.CtxError(ctx, "ScheduledTask [%s] run error: %v, cost: %v", b.name, err, time.Since(startTime))
+					duration := time.Since(startTime)
+					logs.CtxError(ctx, "ScheduledTask [%s] run error: %v, cost: %v", b.name, err, duration)
 				} else {
-					logs.CtxInfo(ctx, "ScheduledTask [%s] run success, cost: %v", b.name, time.Since(startTime))
+					duration := time.Since(startTime)
+					logs.CtxInfo(ctx, "ScheduledTask [%s] run success, cost: %v", b.name, duration)
 				}
 			case <-b.stopChan:
 				return
