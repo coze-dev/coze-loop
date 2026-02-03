581107ccf02c619b1087ef2c9bb088729dd6d7e6
chenzhe.29 <chenzhe.29@bytedance.com>
2026-02-03 19:25:37 +0800

    [feat][backend] add preset model

--- idl/thrift/volcengine/agentkit/llm/volcengine.agentkit.llm.commercial.thrift
+++ idl/thrift/volcengine/agentkit/llm/volcengine.agentkit.llm.commercial.thrift
@@ -29,6 +29,7 @@ struct ListModelsRequest {
     1: optional i64 workspace_id (api.js_conv = 'true', vt.not_nil = 'true', vt.gt = '0', go.tag = 'json:"workspace_id"', api.query = 'WorkspaceId')
     2: optional common.Scenario scenario
     3: optional coze.loop.llm.manage.Filter filter
+    4: optional bool preset_model
     100: optional string cookie (api.header = 'cookie')
     127: optional i32 page_size
     128: optional string page_token
@@ -42,6 +43,7 @@ struct GetModelRequest {
     2: optional i64 model_id (api.js_conv = 'true', api.query = 'ModelId')
     3: optional string identification
     4: optional manage.Protocol protocol
+    5: optional bool preset_model
     100: optional string cookie (api.header = 'cookie')
     250: optional string project_name (api.query = 'ProjectName')
     255: optional base.Base Base
