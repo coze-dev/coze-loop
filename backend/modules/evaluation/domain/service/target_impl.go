d1dfd3c2298e7101e6460354a7df8f66ede7d701
dushaofeng <dushaofeng@bytedance.com>
2026-02-04 11:51:54 +0800

    fix
    Change-Id: I33cb9fbe45291454860f6ffa99f78d69c92c1e8c

--- backend/modules/evaluation/domain/service/target_impl.go
+++ backend/modules/evaluation/domain/service/target_impl.go
@@ -725,26 +725,41 @@ func toTraceParts(ctx context.Context, content *entity.Content) []*tracespec.Mod
 			Type: tracespec.ModelMessagePartType(content.GetContentType()),
 		}}
 	case entity.ContentTypeImage:
+		var name, url string
+		if content.Image != nil {
+			name = gptr.Indirect(content.Image.Name)
+			url = gptr.Indirect(content.Image.URL)
+		}
 		return []*tracespec.ModelMessagePart{{
 			ImageURL: &tracespec.ModelImageURL{
-				Name: gptr.Indirect(content.Image.Name),
-				URL:  gptr.Indirect(content.Image.URL),
+				Name: name,
+				URL:  url,
 			},
 			Type: tracespec.ModelMessagePartType(content.GetContentType()),
 		}}
 	case entity.ContentTypeAudio:
+		var name, url string
+		if content.Audio != nil {
+			name = gptr.Indirect(content.Audio.Name)
+			url = gptr.Indirect(content.Audio.URL)
+		}
 		return []*tracespec.ModelMessagePart{{
 			AudioURL: &tracespec.ModelAudioURL{
-				Name: gptr.Indirect(content.Audio.Name),
-				URL:  gptr.Indirect(content.Audio.URL),
+				Name: name,
+				URL:  url,
 			},
 			Type: tracespec.ModelMessagePartTypeAudio,
 		}}
 	case entity.ContentTypeVideo:
+		var name, url string
+		if content.Video != nil {
+			name = gptr.Indirect(content.Video.Name)
+			url = gptr.Indirect(content.Video.URL)
+		}
 		return []*tracespec.ModelMessagePart{{
 			VideoURL: &tracespec.ModelVideoURL{
-				Name: gptr.Indirect(content.Video.Name),
-				URL:  gptr.Indirect(content.Video.URL),
+				Name: name,
+				URL:  url,
 			},
 			Type: tracespec.ModelMessagePartTypeVideo,
 		}}
