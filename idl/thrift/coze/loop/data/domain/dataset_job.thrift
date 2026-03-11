fb79e81c4d3502fb97ea2aebfc7bcfaf78c4400d
zhuangjiaqi.777 <zhuangjiaqi.777@bytedance.com>
2026-03-11 16:14:57 +0800

    feat: mmodal optimize

--- idl/thrift/coze/loop/data/domain/dataset_job.thrift
+++ idl/thrift/coze/loop/data/domain/dataset_job.thrift
@@ -101,6 +101,7 @@ struct DatasetIOJob {
 
 struct DatasetIOJobOption {
     1: optional bool overwrite_dataset // 覆盖数据集
+    2: optional list<dataset.FieldWriteOption> fieldWriteOptions
 }
 
 struct DatasetIOJobProgress {
