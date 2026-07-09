56c0ee24d22c7de9d39ebc341c49c0711b70269b
xueyizheng <@bytedance.com>
2026-07-09 21:24:20 +0800

    chore(eval): restore standard output internal routes

--- backend/api/router/coze/loop/apis/coze.loop.apis.go
+++ backend/api/router/coze/loop/apis/coze.loop.apis.go
@@ -278,6 +278,17 @@ func Register(r *server.Hertz, handler *apis.APIHandler) {
 						_results := _expt_id.Group("/results", _resultsMw(handler)...)
 						_results.POST("/export", append(_exportexptresultMw(handler), apis.ExportExptResult)...)
 					}
+					{
+						_runs := _expt_id.Group("/runs", _runsMw(handler)...)
+						{
+							_expt_run_id := _runs.Group("/:expt_run_id", _expt_run_idMw(handler)...)
+							{
+								_standard_eval_outputs := _expt_run_id.Group("/standard_eval_outputs", _standard_eval_outputsMw(handler)...)
+								_standard_eval_outputs.POST("/batch_get", append(_mgetexperimentstandardevaloutputsMw(handler), apis.MGetExperimentStandardEvalOutputs)...)
+								_standard_eval_outputs.POST("/list", append(_listexperimentstandardevaloutputsMw(handler), apis.ListExperimentStandardEvalOutputs)...)
+							}
+						}
+					}
 					_experiments.PATCH("/:expt_id", append(_updateexperimentMw(handler), apis.UpdateExperiment)...)
 					_experiments.POST("/list", append(_listexperimentsMw(handler), apis.ListExperiments)...)
 					_experiments.POST("/submit", append(_submitexperimentMw(handler), apis.SubmitExperiment)...)
@@ -593,23 +604,6 @@ func Register(r *server.Hertz, handler *apis.APIHandler) {
 					{
 						_evaluator_version_id0 := _evaluators_versions0.Group("/:evaluator_version_id", _evaluator_version_id0Mw(handler)...)
 						_evaluator_version_id0.POST("/run", append(_runevaluatoroapiMw(handler), apis.RunEvaluatorOApi)...)
-					}
-				}
-				{
-					_experiments1 := _evaluation0.Group("/experiments", _experiments1Mw(handler)...)
-					{
-						_expt_id0 := _experiments1.Group("/:expt_id", _expt_id0Mw(handler)...)
-						{
-							_runs := _expt_id0.Group("/runs", _runsMw(handler)...)
-							{
-								_expt_run_id := _runs.Group("/:expt_run_id", _expt_run_idMw(handler)...)
-								{
-									_standard_eval_outputs := _expt_run_id.Group("/standard_eval_outputs", _standard_eval_outputsMw(handler)...)
-									_standard_eval_outputs.POST("/batch_get", append(_mgetexperimentstandardevaloutputsMw(handler), apis.MGetExperimentStandardEvalOutputs)...)
-									_standard_eval_outputs.POST("/list", append(_listexperimentstandardevaloutputsMw(handler), apis.ListExperimentStandardEvalOutputs)...)
-								}
-							}
-						}
 					}
 				}
 			}
